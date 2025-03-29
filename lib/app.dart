be7cb710ec55e7f2d6c2c0e813da62af689dcc4f
Viktor Tykhonov <viktortykh@gmai.com>
2025-03-29 17:08:15 +0100

    Program with bibliotek bloc

--- lib/app.dart
+++ lib/app.dart
@@ -1,54 +1,10 @@
 import 'package:flutter/material.dart';
-import 'counter/view/counter_page.dart';
-import 'counter/view/empty.dart';
+import 'package:flutter_counter/counter/counter.dart';
 
-
-class CounterApp extends StatelessWidget {
-  const CounterApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const BottomNavigationBarExample(),
-    );
-  }
-}
-
-class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
-
-  @override
-  _BottomNavigationBarExampleState createState() => _BottomNavigationBarExampleState();
-}
-
-class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _pages = <Widget>[
-    const CounterPage(),
-    const EmptyPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Навигация')),
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.exposure_plus_1), label: 'Счетчик'),
-          BottomNavigationBarItem(icon: Icon(Icons.clear), label: 'Пусто'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
+/// {@template counter_app}
+/// A [MaterialApp] which sets the `home` to [CounterPage].
+/// {@endtemplate}
+class CounterApp extends MaterialApp {
+  /// {@macro counter_app}
+  const CounterApp({super.key}) : super(home: const CounterPage());
 }
